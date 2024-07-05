d36c139580f2505ee238cc5a8fb58a1228f0eb7e
Kevin-512 <108360149+Kevin-512@users.noreply.github.com>
2024-07-05 12:40:05 +0100

    Changed navigation to HashRouter

--- vext-games/src/index.js
+++ vext-games/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
 
   </React.StrictMode>
 );
