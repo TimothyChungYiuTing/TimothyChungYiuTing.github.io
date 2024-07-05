1620362e74f7155ec0253de85813dfe06848c2c6
Kevin-512 <108360149+Kevin-512@users.noreply.github.com>
2024-07-05 12:29:13 +0100

    Change href links to use React Router instead

--- vext-games/src/components/NavigationBar.js
+++ vext-games/src/components/NavigationBar.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from "react";
 import VextGamesLogo from "../images/VextGameLogo.png";
+import { Link } from "react-router-dom";
 
 const navigation = [
   { name: "Vext Games", href: "/" },
@@ -10,29 +11,29 @@ const navigation = [
 const NavigationBar = () => {
   return (
     <nav
-          className="flex items-center justify-between p-6 lg:px-12"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Vext Games</span>
-              <img className="h-20 w-auto" src={VextGamesLogo} alt="" />
-            </a>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-2xl font-semibold leading-6 text-white text-outline neon-text"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
-        </nav>
-  )
-}
+      className="flex items-center justify-between p-6 lg:px-12"
+      aria-label="Global"
+    >
+      <div className="flex lg:flex-1">
+        <Link to="/" className="-m-1.5 p-1.5">
+          <span className="sr-only">Vext Games</span>
+          <img className="h-20 w-auto" src={VextGamesLogo} alt="" />
+        </Link>
+      </div>
+      <div className="hidden lg:flex lg:gap-x-12">
+        {navigation.map((item) => (
+          <Link
+            key={item.name}
+            to={item.href}
+            className="text-2xl font-semibold leading-6 text-white text-outline neon-text"
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+      <div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+    </nav>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
